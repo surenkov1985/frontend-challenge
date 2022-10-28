75915888b60742536d3f1b61486e3d22a75a04fb
mich-man <88162731+surenkov1985@users.noreply.github.com>
2022-10-28 23:53:34 +0300

    получение данных, верстка страницы с карточками

--- src/assets/components/pages/AllCats.js
+++ src/assets/components/pages/AllCats.js
@@ -1,21 +1,39 @@
-import React, { useEffect } from "react";
-
-function getData() {
-	fetch("https://api.thecatapi.com/v1/images/search?limit=15&page=0&api_key=live_wgTVl4q73CIJTgweqoNx741qVwh7OXb3haDqoKRm8y1Bw2oi3YREmLHTxZaCVj9X")
-		.then((res) => {
-			return res.json();
-		})
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-}
+import React, { useEffect, useState } from "react";
 
 export const AllCats = () => {
+	const [catsData, setCatsData] = useState([]);
+
+	function getData() {
+		fetch(
+			"https://api.thecatapi.com/v1/images/search?limit=15&page=0&api_key=live_wgTVl4q73CIJTgweqoNx741qVwh7OXb3haDqoKRm8y1Bw2oi3YREmLHTxZaCVj9X"
+		)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setCatsData(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	console.log(catsData);
+
 	useEffect(() => {
 		getData();
 	}, []);
-	return <div className="container content__container">Cats</div>;
+	return (
+		<div className="container content__container">
+			{catsData.map((cat) => {
+				return (
+					<div className="content__img">
+						<div className="content__img-container" key={cat.id}>
+							<img src={cat.url} alt={cat.id} />
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
